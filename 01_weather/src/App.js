1bd28bf2c472bc1ae3e44f2cdd62f9aec1a6548d
1Dohyeon <dh1072005@gmail.com>
2023-02-15 23:58:22 +0900

    fixed untracked files

--- 01_weather/src/App.js
+++ 01_weather/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import GetApi from './component/getApi';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="appContentWrap">
+        <div className='btns'>
+          <div className='btn1'><a>weather</a></div>
+          <div className='btn2'><a>btn2</a></div>
+          <div className='btn3'><a>btn3</a></div>
+        </div>
+        <GetApi/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
