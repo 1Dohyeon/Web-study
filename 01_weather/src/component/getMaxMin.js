f579dca579fabdd8f4cea59b750c7750abbdf77b
1Dohyeon <dh1072005@gmail.com>
2023-02-16 00:00:30 +0900

    commit

--- 01_weather/src/component/getMaxMin.js
+++
@@ -1,8 +0,0 @@
-import '../style.css';
-import React from 'react';
-
-export default function GetTodayTime(props){
-    return(
-        <div className='todayTimeWeather'>{props.maxtemp}°C</div>
-    );
-}
